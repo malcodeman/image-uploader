f47c12b82be2bf75feefd0e0c5002dab8512f8a0
unknown <26983729+malcodeman@users.noreply.github.com>
2021-01-21 17:36:33 +0100

    Switches to TS

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,4 +1,4 @@
 module.exports = {
-  stories: ["../stories/**/*.stories.js"],
+  stories: ["../stories/**/*.stories.jsx"],
   addons: ["@storybook/addon-links", "@storybook/addon-essentials"],
 };
--- stories/ImageUploader.stories.jsx
+++ stories/ImageUploader.stories.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+
 import ImageUploader from "../src/ImageUploader";
 
 export default {
