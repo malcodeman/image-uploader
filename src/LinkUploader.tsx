6af4db175bb2eb06f47e605b8803dd3189dcf4fd
Amer K <26983729+malcodeman@users.noreply.github.com>
2022-05-13 22:14:51 +0200

    Adds link uploader error message

--- src/LinkUploader.tsx
+++ src/LinkUploader.tsx
@@ -7,6 +7,9 @@ import {
   InputRightElement,
   IconButton,
   Image,
+  FormControl,
+  FormLabel,
+  FormErrorMessage,
 } from "@chakra-ui/react";
 import { FiLink2 } from "react-icons/fi";
 import { useForm } from "react-hook-form";
@@ -14,7 +17,10 @@ import { yupResolver } from "@hookform/resolvers/yup";
 import * as yup from "yup";
 
 const schema = yup.object({
-  url: yup.string().url().required(),
+  url: yup
+    .string()
+    .url("Please enter a valid URL")
+    .required("Please enter a URL"),
 });
 const defaultValues = {
   url: "",
@@ -38,17 +44,27 @@ function LinkUploader() {
       ) : (
         <Container maxW="96">
           <form onSubmit={form.handleSubmit(onSubmit)}>
-            <InputGroup size="md">
-              <Input placeholder="Enter a URL" {...form.register("url")} />
-              <InputRightElement>
-                <IconButton
-                  type="submit"
-                  size="md"
-                  aria-label="Import"
-                  icon={<FiLink2 size={16} />}
+            <FormControl isInvalid={Boolean(form.formState.errors.url)}>
+              <FormLabel htmlFor="url">URL</FormLabel>
+              <InputGroup size="md">
+                <Input
+                  {...form.register("url")}
+                  id="url"
+                  placeholder="Enter a URL"
                 />
-              </InputRightElement>
-            </InputGroup>
+                <InputRightElement>
+                  <IconButton
+                    type="submit"
+                    size="md"
+                    aria-label="Import"
+                    icon={<FiLink2 size={16} />}
+                  />
+                </InputRightElement>
+              </InputGroup>
+              <FormErrorMessage>
+                {form.formState.errors.url?.message}
+              </FormErrorMessage>
+            </FormControl>
           </form>
         </Container>
       )}
